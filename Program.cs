f02bdb71288182854c9b8b82c843e0cfbadad440
dennissulejman <45164504+dennissulejman@users.noreply.github.com>
2018-12-20 15:38:50 +0100

    Updated version using lists with methods optimized. Error handling will come in the next version.

--- Program.cs
+++ Program.cs
@@ -16,314 +16,176 @@ namespace CalendarPlanner
 
         static void Main(string[] args)
         {
-            bool mainMenu = true;
-            while (mainMenu)
-            {
-                mainMenu = MainMenu();
-            }
-        }
-        private static bool MainMenu()
-        {
+            Calendar calendar = new Calendar();
+
             Console.Clear();
             Console.WriteLine("Welcome! What would you like to do?");
             Console.WriteLine("Type the corresponding number then press enter to execute:");
             Console.WriteLine("1: Create a new calendar-planner.");
-            Console.WriteLine("2: Open and view/edit your existing calendar-planner.");
-            Console.WriteLine("3: Edit your existing calendar-planner.");
+            Console.WriteLine("2: Add Plans.");
+            Console.WriteLine("3: Remove Plans.");
+            Console.WriteLine("4: Display your existing calendar-planner.");
             Console.WriteLine();
-            Console.WriteLine("4: Close the application.");
+            Console.WriteLine("5: Close the application.");
             string response = Console.ReadLine();
             if (response == "1")
             {
-                CreatePlanner();
-                return true;
+                CreateCalendarPlanner();
+                AddPlan(ref calendar);
+                Main(args);
             }
             else if (response == "2")
             {
-                ReadPlanner();
-                return true;
+                AddPlan(ref calendar);
+                Main(args);
             }
             else if (response == "3")
             {
-                EditPlanner();
-                return true;
+                RemovePlan(ref calendar);
+                Main(args);
             }
             else if (response == "4")
             {
-                return false;
+                DisplayCalendarPlanner();
+                Main(args);
             }
-            else
-            {
-                return true;
-            }
+            Console.ReadLine();
+
+
         }
-        public static void CreatePlanner()
+        static void CreateCalendarPlanner()
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-
-            Calendar calendar = new Calendar();
-            Dictionary<DateTime, string> myCalendar = calendar.GetCalendar();
-            File.ReadAllLines(path + @"\Calendar.csv");
-            Console.Clear();
-            bool add = true;
-            while (add)
-            {
-
-                Console.WriteLine("What are you planning to do?");
-                string Activity = Console.ReadLine();
-                Console.WriteLine("When are you planning to do it?(dd/mm/yy/)");
-                string Date = Console.ReadLine();
-
-                calendar.calendar.Add(DateTime.Parse(Date), Activity);
-                Console.WriteLine("Are you done planning?(y/n)");
-                string response = Console.ReadLine();
-                if (response == "n")
-                {
-                    add = true;
-                }
-                else if (response == "y")
-                {
-                    add = false;
-                    Console.Clear();
-                    Console.WriteLine("Here are your plans for the year:");
-                    Console.WriteLine();
-                    StringBuilder Plans = new StringBuilder();
-                    Plans.AppendLine("My Calendar:");
-                    Plans.AppendLine();
-                    var dates = myCalendar.Select(p => p.Key + "," + p.Value.ToString());
-                    foreach (KeyValuePair<DateTime, string> p in myCalendar.OrderBy(p => p.Key))
-                    {
-                        Console.WriteLine($"{p.Key}: {p.Value}");
-                        Plans.AppendLine($"{p.Key}: {p.Value}".ToString());
-                        File.WriteAllText(path + @"\Calendar.csv", Plans.ToString());
-                    }
-                    Console.ReadLine();
-                }
-
-            }
+            FileStream createNewPlanner = File.Create(path + @"\Calendar.csv");
+            createNewPlanner.Close();
         }
-        public static void ReadPlanner()
+
+        static void GetCalendarPlanner(ref Calendar calendar)
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Console.Clear();
-            string read = File.ReadAllText(path + @"\Calendar.csv");
-            Console.Write(read);
-            Console.WriteLine();
-            Console.WriteLine("Would you like to edit your calendar-planner?(y/n)");
-            string response = Console.ReadLine();
-            if (response == "y")
-            {
-                EditPlanner();
-            }
-        }
-        public static bool EditPlanner()
-        {
-            Console.Clear();
-            Console.WriteLine("How would you like to edit your calendar-planer?");
-            Console.WriteLine("Type the corresponding number then press enter to execute:");
-            Console.WriteLine("1: Add new plans.");
-            Console.WriteLine("2: Edit existing plans.");
-            Console.WriteLine("3: Remove existing plans.");
-            Console.WriteLine();
-            Console.WriteLine("4: Return to main menu.");
-            string response = Console.ReadLine();
-            if (response == "1")
-            {
-                AddPlan();
-                return true;
-            }
-            else if (response == "2")
-            {
-                EditPlan();
-                return true;
-            }
-            else if (response == "3")
-            {
-                RemovePlan();
-                return true;
-            }
-            else if (response == "4")
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        public static void AddPlan()
-        {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Console.Clear();
             StreamReader calendarReader = new StreamReader(path + @"\Calendar.csv");
-            Calendar calendar = new Calendar();
-            Dictionary<DateTime, string> myCalendar = calendar.GetCalendar();
             using (calendarReader)
+
             {
                 calendarReader.ReadLine();
                 calendarReader.ReadLine();
                 while (calendarReader.Peek() != -1)
                 {
                     string line = calendarReader.ReadLine();
-                    string[] header = line.Split(':');
-                    string[] columns = line.Split(' ');
+                    string[] columns = line.Split('-');
 
-                    myCalendar.Add(DateTime.Parse(columns[0]), columns[3]);
+                    calendar.AddPlanEntry(columns[0], columns[1], columns[2]);
                 }
             }
             calendarReader.Close();
-            Console.Clear();
+
+        }
+        public static void DisplayCalendarPlanner()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string read = File.ReadAllText(path + @"\Calendar.csv");
+            Console.WriteLine(read);
+        }
+
+        public static void SaveCalendarPlanner(ref Calendar calendar)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            Console.WriteLine("Here are your new plans for the year:");
+            Console.WriteLine();
+            StringBuilder Plans = new StringBuilder();
+            Plans.AppendLine("My Calendar:");
+            Plans.AppendLine();
+
+            foreach (Plan plan in calendar.Plans.OrderBy(v => v.StartTime))
+            {
+                Console.WriteLine(plan.Display);
+                Plans.AppendLine(plan.Display);
+                File.WriteAllText(path + @"\Calendar.csv", Plans.ToString());
+            }
+        }
+
+
+        public static void AddPlan(ref Calendar calendar)
+        {
+            GetCalendarPlanner(ref calendar);
             bool add = true;
             while (add)
             {
-
+                Console.Clear();
                 Console.WriteLine("What are you planning to do?");
-                string Activity = Console.ReadLine();
-                Console.WriteLine("When are you planning to do it?(dd/mm/yy/)");
-                string Date = Console.ReadLine();
+                string activity = Console.ReadLine();
+                Console.WriteLine("Which date and time does it start?(dd/mm/yy hh:mm)");
+                string startTime = Console.ReadLine();
+                Console.WriteLine("Until which date and time does it last?(dd/mm/yy hh:mm)");
+                string endTime = Console.ReadLine();
+                calendar.AddPlanEntry(startTime, endTime, activity);
 
-                myCalendar.Add(DateTime.Parse(Date), Activity);
                 Console.WriteLine("Are you done adding plans?(y/n)");
                 string response = Console.ReadLine();
                 if (response == "n")
                 {
                     add = true;
+                    Console.Clear();
                 }
                 else if (response == "y")
                 {
                     add = false;
-                    Console.Clear();
-                    Console.WriteLine("Here are your new plans for the year:");
-                    Console.WriteLine();
-                    StringBuilder Plans = new StringBuilder();
-                    Plans.AppendLine("My Calendar:");
-                    Plans.AppendLine();
-                    var dates = myCalendar.Select(p => p.Key + p.Value.ToString());
-                    foreach (KeyValuePair<DateTime, string> p in myCalendar.OrderBy(p => p.Key))
-                    {
-                        Console.WriteLine($"{p.Key}: {p.Value}");
-                        Plans.AppendLine($"{p.Key}: {p.Value}".ToString());
-                        File.WriteAllText(path + @"\Calendar.csv", Plans.ToString());
-                    }
-                    Console.ReadLine();
                 }
-
+                SaveCalendarPlanner(ref calendar);
             }
         }
-        public static void RemovePlan()
+        public static void RemovePlan(ref Calendar calendar)
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Console.Clear();
-            StreamReader calendarReader = new StreamReader(path + @"\Calendar.csv");
-            Calendar calendar = new Calendar();
-            Dictionary<DateTime, string> myCalendar = calendar.GetCalendar();
-            using (calendarReader)
-            {
-                calendarReader.ReadLine();
-                calendarReader.ReadLine();
-                while (calendarReader.Peek() != -1)
-                {
-                    string line = calendarReader.ReadLine();
-                    string[] header = line.Split(':');
-                    string[] columns = line.Split(' ');
-
-                    myCalendar.Add(DateTime.Parse(columns[0]), columns[3]);
-                }
-            }
-            calendarReader.Close();
-            Console.Clear();
+            GetCalendarPlanner(ref calendar);
             bool remove = true;
             while (remove)
             {
+                Console.Clear();
 
-                Console.WriteLine("When is the plan you would like to remove??(dd/mm/yy/)");
-                string Date = Console.ReadLine();
-
-                myCalendar.Remove(DateTime.Parse(Date));
+                Console.WriteLine("When is the plan you would like to remove?");
+                string removeDate = Console.ReadLine();
+                Console.WriteLine("Which plan would you like to remove?");
+                string activity = Console.ReadLine();
+                calendar.RemovePlanEntry(removeDate, activity);
                 Console.WriteLine("Are you done removing plans?(y/n)");
                 string response = Console.ReadLine();
                 if (response == "n")
                 {
                     remove = true;
+                    Console.Clear();
                 }
                 else if (response == "y")
                 {
                     remove = false;
-                    Console.Clear();
-                    Console.WriteLine("Here are your new plans for the year:");
-                    Console.WriteLine();
-                    StringBuilder Plans = new StringBuilder();
-                    Plans.AppendLine("My Calendar:");
-                    Plans.AppendLine();
-                    var dates = myCalendar.Select(p => p.Key + p.Value.ToString());
-                    foreach (KeyValuePair<DateTime, string> p in myCalendar.OrderBy(p => p.Key))
-                    {
-                        Console.WriteLine($"{p.Key}: {p.Value}");
-                        Plans.AppendLine($"{p.Key}: {p.Value}".ToString());
-                        File.WriteAllText(path + @"\Calendar.csv", Plans.ToString());
-                    }
-                    Console.ReadLine();
                 }
-
+                SaveCalendarPlanner(ref calendar);
             }
         }
-        public static void EditPlan()
+        public static void EditPlan(ref Calendar calendar)
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Console.Clear();
-            StreamReader calendarReader = new StreamReader(path + @"\Calendar.csv");
-            Calendar calendar = new Calendar();
-            Dictionary<DateTime, string> myCalendar = calendar.GetCalendar();
-            using (calendarReader)
-            {
-                calendarReader.ReadLine();
-                calendarReader.ReadLine();
-                while (calendarReader.Peek() != -1)
-                {
-                    string line = calendarReader.ReadLine();
-                    string[] header = line.Split(':');
-                    string[] columns = line.Split(' ');
-
-                    myCalendar.Add(DateTime.Parse(columns[0]), columns[3]);
-                }
-            }
-            calendarReader.Close();
-            Console.Clear();
+            GetCalendarPlanner(ref calendar);
             bool edit = true;
             while (edit)
             {
+                Console.Clear();
 
-                Console.WriteLine("When is the plan you would like to edit?(dd/mm/yy/)");
-                string Date = Console.ReadLine();
-                Console.WriteLine("What are you planning to do instead?");
-                string Activity = Console.ReadLine();
-
-                myCalendar[DateTime.Parse(Date)] = Activity;
-                Console.WriteLine("Are you done editing plans?(y/n)");
+                Console.WriteLine("When is the plan you would like to edit?");
+                string removeDate = Console.ReadLine();
+                Console.WriteLine("Which plan would you like to remove?");
+                string activity = Console.ReadLine();
+                calendar.RemovePlanEntry(removeDate, activity);
+                Console.WriteLine("Are you done removing plans?(y/n)");
                 string response = Console.ReadLine();
                 if (response == "n")
                 {
                     edit = true;
+                    Console.Clear();
                 }
                 else if (response == "y")
                 {
                     edit = false;
-                    Console.Clear();
-                    Console.WriteLine("Here are your new plans for the year:");
-                    Console.WriteLine();
-                    StringBuilder Plans = new StringBuilder();
-                    Plans.AppendLine("My Calendar:");
-                    Plans.AppendLine();
-                    var dates = myCalendar.Select(p => p.Key + p.Value.ToString());
-                    foreach (KeyValuePair<DateTime, string> p in myCalendar.OrderBy(p => p.Key))
-                    {
-                        Console.WriteLine($"{p.Key}: {p.Value}");
-                        Plans.AppendLine($"{p.Key}: {p.Value}".ToString());
-                        File.WriteAllText(path + @"\Calendar.csv", Plans.ToString());
-                    }
-                    Console.ReadLine();
                 }
-
+                SaveCalendarPlanner(ref calendar);
             }
         }
     }
